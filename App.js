f870ccb8f30941824fe34623fbe486abe70fd682
Arogya Koirala <arogyakoirala@gmail.com>
2021-01-12 15:41:01 +0545

    first commit

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Linking,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("<DUMMY_TOKEN>");
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  ".mapboxgl-ctrl-logo": {
+    display: "none",
+  },
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "tomato",
+  },
+  map: {
+    flex: 1,
+  },
+
+  absolute: {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    zIndex: 10000,
+    // height: '100%',
+    // width: 20,
+  },
+
+  logo: {
+    flex: 1,
+    width: 100,
+    height: 40,
+    resizeMode: "contain",
+  },
+});
+
+export default class App extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView
+            styleURL="https://api.baato.io/api/v1/styles/breeze?key=bpk.P2eVWKZSIcwoNFhQpEqVULZ5U479mRI_fetWlKTRXmu9"
+            style={styles.map}
+            logoEnabled={false}
+            attributionEnabled={false}
+          >
+            <MapboxGL.Camera
+              zoomLevel={12}
+              centerCoordinate={[85.31853583740946, 27.701739466949107]}
+            />
+          </MapboxGL.MapView>
+          <TouchableOpacity
+            onPress={() =>
+              Linking.canOpenURL("https://baato.io")
+                .then((supported) => {
+                  if (!supported) {
+                    console.log("Can't handle url: " + "https://baato.io");
+                  } else {
+                    return Linking.openURL("https://baato.io");
+                  }
+                })
+                .catch((err) => console.error("An error occurred", err))
+            }
+            style={styles.absolute}
+          >
+            <Image
+              source={require("./logo.png")}
+              style={styles.logo}
+              resizeMode="center"
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
